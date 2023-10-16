4ca1cb9ce00811473bcdde9b30f67c65a1014524
Sabitriver <107032831+sabitriver@users.noreply.github.com>
2023-10-16 20:31:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 const request = require('request')
 const config = require("./config.json")
 
-// selam ben novasy
+// selam ben sabitriver
     client.on("ready", () => {
         client.user.setPresence({ activity: { name: config.durum }, status: "idle" });
         client.channels.cache.get(config.voicekanal).join().catch(x => x)
@@ -12,34 +12,34 @@ const config = require("./config.json")
 
 
     setInterval(async () => {
-        if(guild.vanityURLCode == config.vanity) {// novasy
+        if(guild.vanityURLCode == config.vanity) {// sabitriver
             let onay = new Discord.MessageEmbed().setAuthor(guild.name, guild.iconURL({dynamic: true})).setColor('RANDOM').setTimestamp().setDescription(`"**${guild.name}**" sunucusunun özel url'si belirtildiği gibi **\`${config.vanity}\`** olarak değiştirildi :white_check_mark:`)
-                let kanal = await guild.channels.cache.get(config.urllog)// novasy
+                let kanal = await guild.channels.cache.get(config.urllog)// sabitriver
                      await kanal.send(onay)
-                        await kanal.send(`<@${config.botOwner}>`)// novasy
+                        await kanal.send(`<@${config.botOwner}>`)// sabitriver
 
 
     console.log(`${guild.name} = "${guild.vanityURLCode}" Durum: ✔`) 
-        console.log(`Bottan Çıkış Yapıldı.`)// novasy
+        console.log(`Bottan Çıkış Yapıldı.`)// sabitriver
             process.exit()
-                } else {// novasy
-                    novasyyyy(config.vanity, config.guild, config.token)
-                         }}, 1*500)})// novasy
+                } else {// sabitriver
+                    sabitriveryyy(config.vanity, config.guild, config.token)
+                         }}, 1*500)})// sabitriver
 
-    async function novasyyyy(vanity, token) {
-        const spammer = {//novasy
+    async function sabitriveryyy(vanity, token) {
+        const spammer = {//sabitriver
             url: `https://discord.com/api/v8/guilds/${config.sunucu}/vanity-url`,
                 body: {
                     code: `${vanity}`},
     json: true,
         method: 'PATCH',
-            headers: {//novasy
+            headers: {//sabitriver
                 "Authorization": `Bot ${config.token}`
                     }
                         };
 
     request(spammer, (err, res, body) => {
-        if (err) {//novasy
+        if (err) {//sabitriver
             return console.log(err)}})}
 
 
